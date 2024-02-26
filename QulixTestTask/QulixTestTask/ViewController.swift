de34647b2a330c02fc37baa4a0c9f076596a9620
pshkltsk <pshkltsk@gmail.com>
2024-02-26 14:59:21 +0300

    Created base project, removed storyboard

---
+++ QulixTestTask/QulixTestTask/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  QulixTestTask
+//
+//  Created by Pavel Kylitsky on 26/02/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+        
+    }
+
+
+}
+
