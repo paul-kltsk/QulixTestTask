8cff6454cec0ee614677e636ffb8e8da2ae6bd70
pshkltsk <pshkltsk@gmail.com>
2024-02-27 18:31:43 +0300

    Added test of check field for key in Secrets.plist

--- QulixTestTask/QulixTestTaskTests/APIKeyServiceTest.swift
+++ QulixTestTask/QulixTestTaskTests/APIKeyServiceTest.swift
@@ -15,5 +15,21 @@ final class APIKeyServiceTest: XCTestCase {
         XCTAssertNotNil(Bundle.main.path(forResource: "Secrets", ofType: "plist"))
     }
     
+    // MARK: - Case 2: Test field OpenWeatherMapAPIKey in Secrets.plist
+    func testAPIKeyFieldExists() {
+           // path to Secrets.plist
+        guard let path = Bundle.main.path(forResource: "Secrets", ofType: "plist") else {
+            XCTFail("Secrets.plist file not found")
+            return
+        }
+           
+           // get Secrets.plist dictionary
+        guard let plist = NSDictionary(contentsOfFile: path) else {
+            XCTFail("Unable to read Secrets.plist")
+            return
+        }
+           // cheking OpenWeatherMapAPIKey filed in dictionary
+        XCTAssertNotNil(plist["OpenWeatherMapAPIKey"], "Field OpenWeatherMapAPIKey not found in Secrets.plist")
+    }
 
 }
